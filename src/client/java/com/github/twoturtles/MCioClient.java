9272e0bdf927e243cfdaff6f352b4083f1a4ccd7
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-10-28 13:03:03 -0400

    start

---
+++ src/client/java/com/github/twoturtles/MCioClient.java
@@ -0,0 +1,10 @@
+package com.github.twoturtles;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCioClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
