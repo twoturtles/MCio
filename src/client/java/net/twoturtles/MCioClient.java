85e93649ffdcc4df903a58d18290600cfdb0b162
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-10-30 19:21:56 -0400

    add step keys

--- src/client/java/net/twoturtles/MCioClient.java
+++ src/client/java/net/twoturtles/MCioClient.java
@@ -16,23 +16,29 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
+class MCIO_CONST {
+	public static final String KEY_CATEGORY = "MCio";
+}
+
 public class MCioClient implements ClientModInitializer {
 	/* screen capture */
 	private static final Logger LOGGER = LogUtils.getLogger();
 	private static final MCioFrameCapture fcap = new MCioFrameCapture();
+	private static final MCioIO mcio = new MCioIO();
 
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		LOGGER.info("Client Init");
+
 		fcap.initialize();
+		mcio.initialize();
 	}
 }
 
 class MCioFrameCapture {
 	private static final Logger LOGGER = LogUtils.getLogger();
 	private static KeyBinding captureKey;
-	private static final String CATEGORY = "key.categories.framecapture";
 	private static int frameCount = 0;
 
 	public void initialize() {
@@ -40,9 +46,9 @@ class MCioFrameCapture {
 
 		// Register the keybinding (default to F8)
 		captureKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.mcio.framecapture",
+				"MCioFrameCapture",
 				GLFW.GLFW_KEY_F8,
-				CATEGORY
+				MCIO_CONST.KEY_CATEGORY
 		));
 
 		// Register the tick event
@@ -65,5 +71,46 @@ class MCioFrameCapture {
 				client.getFramebuffer(),
 				(message) -> {}       // No message to the UI
 		);
+	}
+}
+
+class MCioIO {
+	private static final Logger LOGGER = LogUtils.getLogger();
+	private static KeyBinding breakKey, nextKey, contKey;
+	private static int tickCount = 0;
+
+	public void initialize() {
+		LOGGER.info("Init");
+
+		breakKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"MCioIO_break",
+				GLFW.GLFW_KEY_B,
+				MCIO_CONST.KEY_CATEGORY
+		));
+		nextKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"MCioIO_next",
+				GLFW.GLFW_KEY_N,
+				MCIO_CONST.KEY_CATEGORY
+		));
+		contKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"MCioIO_continue",
+				GLFW.GLFW_KEY_C,
+				MCIO_CONST.KEY_CATEGORY
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			tickCount++;
+			if (breakKey.wasPressed() && client.world != null) {
+				LOGGER.info("BREAK {}", tickCount);
+				client.player.networkHandler.sendCommand("tick freeze");
+			}
+			if (nextKey.wasPressed() && client.world != null) {
+				LOGGER.info("NEXT {}", tickCount);
+				client.player.networkHandler.sendCommand("tick step");
+			}
+			if (contKey.wasPressed() && client.world != null) {
+				LOGGER.info("CONT {}", tickCount);
+				client.player.networkHandler.sendCommand("tick unfreeze");
+			}
+		});
 	}
 }
