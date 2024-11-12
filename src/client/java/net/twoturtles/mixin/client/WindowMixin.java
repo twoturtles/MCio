2c349dbc7a352fb97cc43f2a60c5bc6dfe54fea7
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-11-12 15:04:13 -0500

    add sample code from Sodium Extra to avoid double size frame on mac.
    Missing something. Doesn't work.

--- src/client/java/net/twoturtles/mixin/client/WindowMixin.java
+++ src/client/java/net/twoturtles/mixin/client/WindowMixin.java
@@ -4,8 +4,15 @@ import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.nio.ByteBuffer;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.MinecraftClient;
+
 
 import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
@@ -45,5 +52,32 @@ public class WindowMixin {
         pixelBuffer = null;
         frame = null;
     }
+
+    /* Based on https://github.com/FlashyReese/sodium-extra-fabric/blob/1.21/dev/common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/reduce_resolution_on_mac/MixinWindow.java */
+    // Doesn't quite work.
+//    @Shadow
+//    private int framebufferWidth;
+//
+//    @Shadow
+//    private int framebufferHeight;
+//
+//    @Redirect(at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwDefaultWindowHints()V"),
+//            method = "<init>", remap = false)
+//    private void onDefaultWindowHints() {
+//        GLFW.glfwDefaultWindowHints();
+//        if (MinecraftClient.IS_SYSTEM_MAC) {
+//            GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW.GLFW_FALSE);
+//        }
+//    }
+//
+//    @Inject(at = @At(value = "RETURN"), method = "onFramebufferSizeChanged")
+//    private void afterUpdateFrameBufferSize(CallbackInfo ci) {
+//        // prevents mis-scaled startup screen
+//        if (MinecraftClient.IS_SYSTEM_MAC) {
+//            framebufferWidth /= 2;
+//            framebufferHeight /= 2;
+//        }
+//    }
+//
 
 }
