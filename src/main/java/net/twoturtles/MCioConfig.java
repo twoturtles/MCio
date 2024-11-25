e826bd1946ac38a75c7aeebff7ccba7606bf69f1
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-11-24 19:34:19 -0500

    get mode from env

--- src/main/java/net/twoturtles/MCioConfig.java
+++ src/main/java/net/twoturtles/MCioConfig.java
@@ -1,19 +1,44 @@
 package net.twoturtles;
 
+// MCio Config class - command line, env, disk (TODO)
 public class MCioConfig {
-    private static MCioConfig instance;
     public MCioDef.Mode mode;
 
     public static MCioConfig getInstance() {
-        if (instance == null) {
-            instance = new MCioConfig();
-        }
-        return instance;
+        return LazyHolder.INSTANCE;
+    }
+
+    // Lazy initialization holder class
+    private static class LazyHolder {
+        static final MCioConfig INSTANCE = new MCioConfig();
     }
 
     private MCioConfig() {
-        // XXX Load from disk
+        loadDefaults();
+        loadFromDisk();
+        loadFromEnv();
+        loadFromCommandLine();
+    }
+
+    private void loadDefaults() {
         mode = MCioDef.Mode.OFF;
     }
-}
 
+    // TODO
+    private void loadFromDisk() { }
+    private void loadFromEnv() {
+        mode = MCioDef.Mode.fromString(System.getenv("MCIO_MODE"));
+    }
+    // TODO
+    private void loadFromCommandLine() { }
+
+
+    public void save() {
+        // Save config to disk
+    }
+
+    public void reset() {
+        loadDefaults();
+        save();
+    }
+}
--- src/main/java/net/twoturtles/MCioDef.java
+++ src/main/java/net/twoturtles/MCioDef.java
@@ -4,11 +4,25 @@ public class MCioDef {
     public static final String KEY_CATEGORY = "MCio";
 
     public enum Mode {
-        OFF,
-        SYNC,
-        ASYNC
+        OFF("off"),
+        SYNC("sync"),
+        ASYNC("async");
+
+        private final String value;
+
+        Mode(String value) {
+            this.value = value;
+        }
+
+        public static Mode fromString(String str) {
+            if (str == null) return OFF;
+            for (Mode mode : Mode.values()) {
+                if (mode.value.equalsIgnoreCase(str)) {
+                    return mode;
+                }
+            }
+            return OFF;
+        }
     }
 
 }
-
-
