ce444de20bb879cbd19d048f091f8f74e29bda3d
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2025-01-24 00:00:14 -0500

    Add config for disabling fps limits and raw frames

--- src/main/java/net/twoturtles/MCioConfig.java
+++ src/main/java/net/twoturtles/MCioConfig.java
@@ -10,7 +10,7 @@ public class MCioConfig {
         OFF, SYNC, ASYNC;
     }
     public enum MCioFrameType {
-        PNG, JPEG;
+        RAW, PNG, JPEG;
     }
 
     // Constants
@@ -22,6 +22,7 @@ public class MCioConfig {
     public MCioMode mode;
     public MCioFrameType frameType;
     public int frameQuality;
+    public boolean unlimitedFPS;   // Disable Minecraft FPS limiting
     public int actionPort;
     public int observationPort;
     public boolean hideMinecraftWindow;
@@ -29,8 +30,10 @@ public class MCioConfig {
 
     // Defaults
     public static final MCioMode DEFAULT_MCIO_MODE = MCioMode.ASYNC;
-    public static final MCioFrameType DEFAULT_MCIO_FRAME_TYPE = MCioFrameType.PNG;
+    public static final MCioFrameType DEFAULT_MCIO_FRAME_TYPE = MCioFrameType.RAW;
     public static final int DEFAULT_FRAME_QUALITY = 85; // Only used for JPEG frames
+    public static final boolean DEFAULT_UNLIMITED_FPS_SYNC = true;
+    public static final boolean DEFAULT_UNLIMITED_FPS_ASYNC = false;
     public static final int DEFAULT_ACTION_PORT = 4001; // For receiving 4ctions
     public static final int DEFAULT_OBSERVATION_PORT = 8001;    // For sending 8bservations
     public static final boolean DEFAULT_HIDE_MINECRAFT_WINDOW = false;
@@ -47,6 +50,16 @@ public class MCioConfig {
         frameType = getEnvEnum("MCIO_FRAME_TYPE", DEFAULT_MCIO_FRAME_TYPE);
         frameQuality = getEnvInt("MCIO_FRAME_QUALITY", DEFAULT_FRAME_QUALITY);
         frameQuality = Math.clamp(frameQuality, 1, 100);
+
+        // The default depends on the mode. In sync mode we want to go as fast as possible.
+        // Async mode can use however Minecraft is configured.
+        boolean defaultUnlimitedFPS = switch (mode) {
+            case OFF -> true;
+            case SYNC -> DEFAULT_UNLIMITED_FPS_SYNC;
+            case ASYNC -> DEFAULT_UNLIMITED_FPS_ASYNC;
+        };
+        unlimitedFPS = getEnvBoolean("MCIO_UNLIMITED_FPS", defaultUnlimitedFPS);
+
         actionPort = getEnvInt("MCIO_ACTION_PORT", DEFAULT_ACTION_PORT);
         observationPort = getEnvInt("MCIO_OBSERVATION_PORT", DEFAULT_OBSERVATION_PORT);
         hideMinecraftWindow = getEnvBoolean("MCIO_HIDE_WINDOW", DEFAULT_HIDE_MINECRAFT_WINDOW);
@@ -57,6 +70,7 @@ public class MCioConfig {
         if (frameType == MCioFrameType.JPEG) {
             LOGGER.info("MCIO_FRAME_QUALITY={}", frameQuality);
         }
+        LOGGER.info("MCIO_UNLIMITED_FPS={}", unlimitedFPS);
         LOGGER.info("MCIO_ACTION_PORT={}", actionPort);
         LOGGER.info("MCIO_OBSERVATION_PORT={}", observationPort);
         LOGGER.info("MCIO_HIDE_WINDOW={}", hideMinecraftWindow);
