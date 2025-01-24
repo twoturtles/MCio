812afc6a0967935b8aecd3e6a1e955e047a150c4
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2025-01-24 00:03:18 -0500

    add mixin to disable fps limit

---
+++ src/client/java/net/twoturtles/mixin/client/RenderSystemMixin.java
@@ -0,0 +1,22 @@
+package net.twoturtles.mixin.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.twoturtles.MCioConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+/**
+ * Blocks the call to RenderSystem.limitDisplayFPS based on config
+ */
+@Mixin(RenderSystem.class)
+public class RenderSystemMixin {
+    @Inject(method = "limitDisplayFPS", at = @At("HEAD"), cancellable = true)
+    private static void disableLimitDisplayFPS(int fps, CallbackInfo ci) {
+        if (MCioConfig.getInstance().unlimitedFPS) {
+            ci.cancel(); // Don't limit, cancel the call
+        }
+    }
+}
