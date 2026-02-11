07d2e39a9a5f71d4642073c765fefa666a6d8007
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2026-02-11 12:58:25 -0500

    remove todo

--- src/main/java/net/twoturtles/MCioConfig.java
+++ src/main/java/net/twoturtles/MCioConfig.java
@@ -1,5 +1,3 @@
-// TODO(Ravel): Failed to fully resolve file: null cannot be cast to non-null type
-// com.intellij.psi.PsiJavaCodeReferenceElement
 package net.twoturtles;
 
 import com.mojang.logging.LogUtils;
